fc5beacff1351c2e2ac2fc5bc2b97094ff2c8bd7
Yasser Carreón <yacasia@gmail.com>
2016-11-22 17:57:06 -0600

    Added delete and get note methods

--- notes.js
+++ notes.js
@@ -17,24 +17,20 @@ var saveNotes = (notes) => {
 }
 
 var addNote = (title, body) => {
-
   // console.log('Adding new note', title, body);
   var notes = fetchNotes()
   var note = {
     title,
     body
   }
-
   // Checks if duplicated title exists
   var duplicateNotes = notes.filter(note => note.title === title)
-
   // Avoids duplicated note titles
-  if(duplicateNotes.length === 0) {
+  if (duplicateNotes.length === 0) {
     notes.push(note)
     saveNotes(notes)
     return note
   }
-  
 }
 
 var getAll = () => {
@@ -42,16 +38,30 @@ var getAll = () => {
 }
 
 var getNote = (title) => {
-  console.log('Reading note', title);
+  // console.log('Reading note', title);
+  var notes = fetchNotes()
+  var filteredNotes = notes.filter(note => note.title === title)
+  return filteredNotes[0]
 }
 
 var removeNote = (title) => {
-  console.log('Removing note', title);
+  // console.log('Removing note', title);
+  var notes = fetchNotes()
+  var filteredNotes = notes.filter(note => note.title !== title)
+  saveNotes(filteredNotes)
+  return filteredNotes.length !== notes.length
+}
+
+var logNote = (note) => {
+  console.log('----------');
+  console.log('Title:', note.title);
+  console.log('Body:', note.body);
 }
 
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 }
